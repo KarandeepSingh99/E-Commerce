4f60a11bba9d0fa7b2dde984a498b6121a5471b3
Karandeep <karandeep100399@gmail.com>
2023-01-10 13:11:50 +0530

    movie app

---
+++ src/components/RemoveFavourites.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const RemoveFavourites = () => {
+	return (
+		<>
+			<span className='mr-2'>Remove from favourites</span>
+			<svg
+				width='1em'
+				height='1em'
+				viewBox='0 0 16 16'
+				class='bi bi-x-square'
+				fill='currentColor'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<path
+					fill-rule='evenodd'
+					
+				/>
+				<path
+					fill-rule='evenodd'
+				
+				/>
+			</svg>
+		</>
+	);
+};
+
+export default RemoveFavourites;
